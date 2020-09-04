e46ce73e033d2aef885807a86f97f9236c9088f8
Darius <dariuspasca@protonmail.com>
2020-09-04 17:29:50 +0200

    cookies

--- components/ui/TheCookieSnack.vue
+++ components/ui/TheCookieSnack.vue
@@ -7,18 +7,22 @@
       ref="card"
       :style="{ transform: transformString }"
       style="touch-action: none"
-      class="flex xs:w-10/12 sm:w-5/12 md:w-4/12 lg:w-2/12 my-4 bg-white rounded-lg shadow-md px-2 py-4 text-dark-tint justify-start space-x-3 align-middle sm:mr-4 md:mr-4 lg:mr-8 select-none"
+      class="flex xs:w-11/12 sm:w-5/12 md:w-4/12 lg:w-2/12 my-4 bg-medium-tint rounded-lg custom-shadow mx-4 py-1 text-dark justify-start space-x-3 align-middle sm:mr-4 md:mr-4 lg:mr-8 select-none duration-150"
     >
-      <button class="focus:outline-none" @click="bakeCookie()">
-        <svg width="20px" height="20px">
-          <path
-            d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zM7.17 5.76a1 1 0 0 0-1.41 1.41L8.59 10l-2.83 2.83a1 1 0 0 0 1.41 1.41L10 11.41l2.83 2.83a1 1 0 0 0 1.41-1.41L11.41 10l2.83-2.83a1 1 0 1 0-1.41-1.41L10 8.59z"
-          ></path>
-        </svg>
-      </button>
-      <div>
-        <span>Usiamo i</span>
-        <nuxt-link :to="localePath('/cookie-policy')">Cookies</nuxt-link>
+      <div class="flex items-center w-full justify-center space-x-4">
+        <img
+          class="h-16 w-auto"
+          src="~/assets/images/cookie.png"
+          alt="Cookie Cthlhu"
+        />
+        <div class="w-7/12">Usiamo i Cookies per migliorare l'esperienza.</div>
+        <button class="focus:outline-none right-0" @click="bakeCookie()">
+          <svg width="24px" height="24px">
+            <path
+              d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zM7.17 5.76a1 1 0 0 0-1.41 1.41L8.59 10l-2.83 2.83a1 1 0 0 0 1.41 1.41L10 11.41l2.83 2.83a1 1 0 0 0 1.41-1.41L11.41 10l2.83-2.83a1 1 0 1 0-1.41-1.41L10 8.59z"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   </div>
@@ -40,14 +44,18 @@ export default {
   computed: {
     transformString() {
       const { x, y } = this.interactPosition
-      return `translate3D(${x}px, ${y}px, 0)`
+      return `translate(${x}px, ${y}px)`
     },
   },
   mounted() {
     const element = this.$refs.card
 
     interact(element).draggable({
-      inertia: true,
+      inertia: {
+        resistance: 30,
+        minSpeed: 200,
+        endSpeed: 100,
+      },
       modifiers: [
         interact.modifiers.snap({
           targets: [interact.createSnapGrid({ x: 30, y: 30 })],
@@ -100,3 +108,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.custom-shadow {
+  box-shadow: 0px 3px 12px #0000004d;
+}
+</style>
