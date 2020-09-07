ba95a94fc7f074d1bf8bb638e4506dea6b988e45
Darius <dariuspasca@protonmail.com>
2020-09-07 12:33:51 +0200

    cookie time

--- components/ui/TheCookieSnack.vue
+++ components/ui/TheCookieSnack.vue
@@ -7,21 +7,24 @@
       ref="card"
       :style="{ transform: transformString }"
       style="touch-action: none"
-      class="flex xs:w-11/12 sm:w-5/12 md:w-4/12 lg:w-2/12 my-4 bg-medium-tint rounded-lg custom-shadow mx-4 py-1 text-dark justify-start space-x-3 align-middle sm:mr-4 md:mr-4 lg:mr-8 select-none duration-150 pointer-events-none"
+      class="flex xs:w-11/12 sm:w-5/12 md:w-4/12 lg:w-2/12 my-4 bg-medium-tint rounded-lg custom-shadow mx-4 py-1 text-dark justify-start space-x-3 align-middle sm:mr-4 md:mr-4 lg:mr-8 select-none duration-500 lg:duration-1000 cursor-default"
     >
-      <div class="flex items-center w-full justify-center space-x-4">
+      <div
+        class="flex items-center w-full justify-center space-x-4 cursor-default"
+      >
         <img
           class="h-16 w-auto"
           src="~/assets/images/cookie.png"
           alt="Cookie Cthlhu"
         />
-        <div class="w-7/12">Usiamo i Cookies per migliorare l'esperienza.</div>
-        <button
-          class="focus:outline-none right-0 pointer-events-auto"
-          @click="bakeCookie()"
-        >
-          <svg width="24px" height="24px">
+        <div class="w-7/12">
+          <p>Usiamo i Cookies.</p>
+          <p class="underline text-dark-tint">Scopri di più</p>
+        </div>
+        <button class="focus:outline-none right-0" @click="bakeCookie()">
+          <svg class="pointer-events-none" width="24px" height="24px">
             <path
+              class="pointer-events-none"
               d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zM7.17 5.76a1 1 0 0 0-1.41 1.41L8.59 10l-2.83 2.83a1 1 0 0 0 1.41 1.41L10 11.41l2.83 2.83a1 1 0 0 0 1.41-1.41L11.41 10l2.83-2.83a1 1 0 1 0-1.41-1.41L10 8.59z"
             ></path>
           </svg>
@@ -53,52 +56,57 @@ export default {
   mounted() {
     const element = this.$refs.card
 
-    interact(element).draggable({
-      inertia: {
-        resistance: 30,
-        minSpeed: 200,
-        endSpeed: 100,
-      },
-      modifiers: [
-        interact.modifiers.snap({
-          targets: [interact.createSnapGrid({ x: 30, y: 30 })],
-          range: Infinity,
-          relativePoints: [{ x: 0, y: 0 }],
-        }),
-        interact.modifiers.restrict({
-          restriction: element.parentNode,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 0 },
-          endOnly: true,
-        }),
-      ],
-      onmove: (event) => {
-        const x = this.interactPosition.x + event.dx
-        // const y = this.interactPosition.y + event.dy
-        this.interactSetPosition({ x })
-      },
-      onend: () => {
-        if (
-          this.interactPosition.x < 0 ||
-          this.interactPosition.x < window.innerWidth / 3
-        ) {
-          this.resetCardPosition()
-        } else {
-          const x = window.innerWidth + 100
+    if (this.$device.isMobileOrTablet) {
+      interact(element).draggable({
+        inertia: {
+          resistance: 30,
+          minSpeed: 200,
+          endSpeed: 100,
+        },
+        modifiers: [
+          interact.modifiers.snap({
+            targets: [interact.createSnapGrid({ x: 30, y: 30 })],
+            range: Infinity,
+            relativePoints: [{ x: 0, y: 0 }],
+          }),
+          interact.modifiers.restrict({
+            restriction: element.parentNode,
+            elementRect: { top: 0, left: 0, bottom: 1, right: 0 },
+            endOnly: true,
+          }),
+        ],
+        onmove: (event) => {
+          const x = this.interactPosition.x + event.dx
+          // const y = this.interactPosition.y + event.dy
           this.interactSetPosition({ x })
-        }
-      },
-    })
+        },
+        onend: () => {
+          if (
+            this.interactPosition.x < 0 ||
+            this.interactPosition.x < window.innerWidth / 1.5
+          ) {
+            this.resetCardPosition()
+          } else {
+            const x = window.innerWidth + 100
+            this.interactSetPosition({ x })
+          }
+        },
+      })
+    }
   },
   beforeDestroy() {
     interact(this.$refs.card).unset()
   },
   methods: {
     bakeCookie() {
-      this.show = false
+      const x = window.innerWidth + 100
+      this.interactSetPosition({ x })
+      // this.show = false
       // this.$cookies.set('cookie-consent', 'cookie-value', {
       //   path: '/',
       //   maxAge: 60 * 60 * 24 * 7,
       // })
+      console.log('closeButton')
     },
     interactSetPosition(coordinates) {
       const { x = 0, y = 0 } = coordinates
