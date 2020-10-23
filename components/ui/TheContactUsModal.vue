9974a109c42e4784869176ecf4f5657be86f30bd
Darius <dariuspasca@users.noreply.github.com>
2020-10-23 13:40:15 +0200

    contact us modal close button

--- components/ui/TheContactUsModal.vue
+++ components/ui/TheContactUsModal.vue
@@ -10,28 +10,29 @@
     @before-close="beforeClose"
   >
     <v-dialog />
-    <div slot="top-right">
-      <button
-        class="text-white float-right my-3 mr-3 focus:outline-none"
-        @click="$modal.hide('get-link-m8')"
-      >
-        <svg
-          class="fill-current text-dark h-5 w-5"
-          style="enable-background: new 0 0 512 512"
-          viewBox="0 0 512 512"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"
-          />
-        </svg>
-      </button>
-    </div>
-    <h3
-      class="py-2 text-2xl text-dark-shade font-gitan font-medium ml-4 tracking-wide"
+    <div
+      class="flex flex-col"
+      lazy-background="https://storage.googleapis.com/roleever-public-assets/www/background_desaturated.jpg"
     >
-      {{ $t('contact_form.hello') }}
-    </h3>
+      <div slot="top-right">
+        <button
+          class="text-dark float-right my-3 mr-3 focus:outline-none"
+          @click="$modal.hide('send-message')"
+        >
+          <svg
+            class="fill-current text-primary-shade h-5 w-5"
+            style="enable-background: new 0 0 512 512"
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+
     <div class="flex flex-row px-3 pb-6 pt-2">
       <form
         class="contact-form flex flex-col w-full px-3"
