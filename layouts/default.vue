f52fc14dfc28c9d6cb3a87156ccd43802e45b007
Darius <dariuspasca@users.noreply.github.com>
2020-11-25 12:48:27 +0100

    page transition test

--- layouts/default.vue
+++ layouts/default.vue
@@ -95,6 +95,20 @@ export default {
 </script>
 
 <style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+
+.active-link,
+.exact-active-link {
+  font-weight: bold;
+}
+
 .modalOpen {
   overflow: hidden;
   position: fixed;
