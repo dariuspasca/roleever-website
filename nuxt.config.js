ee748b3aaa125e0a7b1a0e4f0b884f791438dbe7
Darius <dariuspasca@users.noreply.github.com>
2020-12-28 12:44:05 +0100

    image optimization

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,6 +79,7 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
+    '@aceforth/nuxt-optimized-images',
   ],
   /*
    ** Nuxt.js modules
